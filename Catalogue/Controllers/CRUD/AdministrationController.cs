01d9496ae2f53a0f6c150914370093a3e290c6a0
Barytkanov <artez994@gmail.com>
2018-01-08 14:40:44 +0600

    Added Controllers

---
+++ Catalogue/Controllers/CRUD/AdministrationController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Catalogue.Controllers.CRUD
+{
+    public class AdministrationController : Controller
+    {
+        // GET: Administration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Administration/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Administration/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Administration/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Administration/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Administration/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Administration/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Administration/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Catalogue/Controllers/CRUD/PositionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Catalogue.Controllers.CRUD
+{
+    public class PositionController : Controller
+    {
+        public ActionResult ReadPosition()
+        {
+            return View();
+        }
+        // GET: Create
+        public ActionResult CreatePosition()
+        {
+            return View();
+        }
+
+        public ActionResult DeletePosition()
+        {
+            return View();
+        }
+
+        public ActionResult UpdatePosition()
+        {
+            return View();
+        }
+
+        
+
+
+    }
+}
