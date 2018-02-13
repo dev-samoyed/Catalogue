02a2046b113e43172b7e2321b48ce9785fa23ee5
VFedyaev <VFedyaev@stat.kg>
2018-02-13 15:44:15 +0600

    Made interface 100

--- Catalogue/Scripts/app.js
+++ Catalogue/Scripts/app.js
@@ -89,40 +89,6 @@ function toPrevMain(from = "") {
         $("#imgPreview").hide()  
     }
 
-    function checkDismissed () {
-        $("#dateDis").on('change', function () {
-            var dis_date = $("#dateDis")[0];
-            var checkbox = $("#checkBox")[0];
-            if(dis_date.value != "")
-            {
-                checkbox.checked = true;
-                checkbox.setAttribute("disabled", true);
-            } else {
-                checkbox.checked = false;
-                checkbox.removeAttribute("disabled");
-            }
-        });
-
-        $("#checkBox").on('click', function () {
-            var dis_date = $("#dateDis")[0];
-         
-            if (this.checked == true) {
-                var now = new Date();
-                var day = ("0" + now.getDate()).slice(-2);
-                var month = ("0" + (now.getMonth() + 1)).slice(-2);
-                var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
-
-                dis_date.value = today;
-                dis_date.setAttribute("readonly", true);
-            } 
-            else if(this.checked == false) {
-                dis_date.value = null;
-                dis_date.removeAttribute("readonly");
-            }
-        
-        });
-    }
-
     function removeBlock() {
         $("#toRemove").remove();
     }
@@ -130,3 +96,18 @@ function toPrevMain(from = "") {
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     })
+
+    $(document).ready(function(){
+        $('#button').click(function(){
+            $(this).attr('disabled', 'disabled');
+            $('form').submit();
+            setTimeout(function () { $('#button').removeAttr('disabled'); }, 1000);
+        });
+    });
+
+    $(window).load(function () {
+        // Animate loader off screen
+        $(".se-pre-con").fadeOut(1000);
+        alert('gg');
+    });
+      
