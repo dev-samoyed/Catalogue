4513f4fff8ae98c7cf18c309f12a4390914f35cd
Barytkanov <artez994@gmail.com>
2018-02-14 09:22:34 +0600

    project repaired

---
+++ Catalogue/Controllers/CRUD/EmployeeController.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Catalogue.Models.Tables;
+using System.Net;
+using System.Data.Entity;
+using PagedList;
+using System.IO;
+using Catalogue.Models;
+using System.Data.Entity.Validation;
+using System.Web.Helpers;
+
+namespace Catalogue.Controllers.CRUD
+{
+    public class EmployeeController : Controller
+    {
+        CatalogueContext db = new CatalogueContext();
+
+        // Ajax pagination PartialView Employee 
+        public ActionResult AjaxPositionList(int? page)
+        {
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return PartialView(db.Employees.Include(e => e.Department).Include(p => p.Position).OrderBy(i => i.EmployeeFullName).ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Employee
+        public ActionResult Index(int? page)
+        {
+            List<Position> positions = db.Positions.ToList();
+            ViewBag.Positions = positions;
+
+            List<Department> departments = db.Departments.ToList();
+            ViewBag.Departments = departments;
+
+            List<Administration> admins = db.Administrations.ToList();
+            ViewBag.Admins = admins;
+
+            List<Division> divisions = db.Divisions.ToList();
+            ViewBag.Divisions = divisions;
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(db.Employees.Include(e => e.Department).Include(p => p.Position).OrderBy(i => i.EmployeeFullName).ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            
+            Employee employee = db.Employees.Include(p => p.Position).Include(d => d.Department).SingleOrDefault(e => e.EmployeeId == id);
+            return View(employee);
+        }
+
+        [HttpGet]
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            SelectList departmentList = new SelectList(db.Departments, "DepartmentId", "DepartmentName");
+            ViewBag.DepartmentList = departmentList;
+            SelectList positionList = new SelectList(db.Positions, "PositionId", "PositionName");
+            ViewBag.PositionList = positionList;
+            return View();
+        }
+        
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(Employee collection, HttpPostedFileBase productImg)
+        {
+            if (ModelState.IsValid)
+            {          
+                if (productImg == null)
+                {
+                    collection.EmployeePhoto = "default-avatar.png";
+                }
+                else
+                {
+                    var fileName = Path.GetFileName(productImg.FileName);
+                    
+                    fileName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + fileName;
+
+                    var directoryToSave = Server.MapPath(Url.Content("~/images"));
+
+                    var pathToSave = Path.Combine(directoryToSave, fileName);
+
+                    productImg.SaveAs(pathToSave);
+                    collection.EmployeePhoto = fileName;
+                }
+            }
+            db.Employees.Add(collection);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+                return HttpNotFound();
+
+            SelectList departmentList = new SelectList(db.Departments, "DepartmentId", "DepartmentName");
+            ViewBag.DepartmentList = departmentList;
+            SelectList positionList = new SelectList(db.Positions, "PositionId", "PositionName");
+            ViewBag.PositionList = positionList;
+            return View(employee);
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Employee collection, HttpPostedFileBase productImg, string photo)
+        {
+            if (ModelState.IsValid)
+            {
+                if (productImg == null)
+                {
+                    collection.EmployeePhoto = photo;
+                }
+                else if (productImg != null)
+                {
+                    string fullPath = Request.MapPath("~/images/" + photo);
+                    if (System.IO.File.Exists(fullPath) && photo != "default-avatar.png")
+                    {
+                        System.IO.File.Delete(fullPath);
+                    }
+
+                    var fileName = Path.GetFileName(productImg.FileName);
+
+                    fileName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + fileName;
+
+                    var directoryToSave = Server.MapPath(Url.Content("~/images"));
+
+                    var pathToSave = Path.Combine(directoryToSave, fileName);
+                    productImg.SaveAs(pathToSave);
+                    collection.EmployeePhoto = fileName;
+                }
+            }
+            db.Entry(collection).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+            Employee employee = db.Employees.Include(p => p.Position).Include(d => d.Department).SingleOrDefault(e => e.EmployeeId == id);
+            if (employee != null)
+                return PartialView("Delete", employee);
+            return View("Index");
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult Delete(int? id, string photoName)
+        {
+            Employee employee = new Employee();
+            try
+            {
+                if (id == null)
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                employee = db.Employees.Find(id);
+                if (employee == null)
+                    return HttpNotFound();
+
+                string fullPath = Request.MapPath("~/images/" + photoName);
+                if (System.IO.File.Exists(fullPath) && photoName != "default-avatar.png")
+                {
+                    System.IO.File.Delete(fullPath);
+                }
+
+                db.Employees.Remove(employee);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Catalogue/Scripts/app.js
@@ -0,0 +1,132 @@
+﻿//on close collapse
+$('.collapse').on('hidden.bs.collapse', function () {
+  var target = '#'+$(this).attr('data-parent');
+  $(target).removeClass('collapse-open');
+});
+
+//on open collapse
+$('.collapse').on('shown.bs.collapse', function () {
+    var target = '#'+$(this).attr('data-parent');
+    $(target).addClass('collapse-open');
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+function hideAccordion() {
+    $("#accordion").hide();
+}
+
+function hideEmployeeList() {
+    $("#employee-list").hide();
+}
+
+function toPrevMain(from = "") {
+    if (from == "list") {
+        $("#employee-list").empty();
+    } else {
+        $("#results").empty();
+    }
+    
+    $("#accordion").show();
+}
+
+    function toPrevEmployeeList() {
+        $("#results").empty();
+        $("#employee-list").show();
+    }
+
+    function removeListAndPagination() {
+        $("#listTable").remove();
+        $("#paginationToDelete").remove();
+    }
+
+    function checkPhotoPreview(){
+        $(document).ready(function () {
+            $("#imageBrowes").change(function () {
+                var File = this.files;
+                if (File && File[0]) {
+                    ReadImage(File[0]);
+                }
+            })
+        })
+
+        var ReadImage = function (file) {
+
+            var reader = new FileReader;
+            var image = new Image;
+            var size = ~~(file.size);
+            var type = file.type;
+
+            if (type != "image/jpeg" && type != "image/png" && type != "image/jpg") {
+                ClearPreview();
+                $('#myModal').modal('show');
+                $('#MessageError').text('Необходимо выбрать фотографию с расширением JPeG или PNG!');   
+            } 
+            else{
+                reader.readAsDataURL(file);
+                reader.onload = function (_file) {
+
+                    image.src = _file.target.result;
+                    image.onload = function () {
+          
+                        $("#targetImg").attr('src', _file.target.result);
+                        $("#imgPreview").show();
+                                 
+                        if (size > 2000000) {
+                            ClearPreview();
+                            $('#myModal').modal('show');
+                            $('#MessageError').text('Размер изображения не должно превышать 2 МБ!');                      
+                        } 
+                    }
+                }
+            }
+        }
+    }
+
+    function ClearPreview () {
+        $("#imageBrowes").val('');
+        $("#imgPreview").hide()  
+    }
+
+    function checkDismissed () {
+        $("#dateDis").on('change', function () {
+            var dis_date = $("#dateDis")[0];
+            var checkbox = $("#checkBox")[0];
+            if(dis_date.value != "")
+            {
+                checkbox.checked = true;
+                checkbox.setAttribute("disabled", true);
+            } else {
+                checkbox.checked = false;
+                checkbox.removeAttribute("disabled");
+            }
+        });
+
+        $("#checkBox").on('click', function () {
+            var dis_date = $("#dateDis")[0];
+         
+            if (this.checked == true) {
+                var now = new Date();
+                var day = ("0" + now.getDate()).slice(-2);
+                var month = ("0" + (now.getMonth() + 1)).slice(-2);
+                var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
+
+                dis_date.value = today;
+                dis_date.setAttribute("readonly", true);
+            } 
+            else if(this.checked == false) {
+                dis_date.value = null;
+                dis_date.removeAttribute("readonly");
+            }
+        
+        });
+    }
+
+    function removeBlock() {
+        $("#toRemove").remove();
+    }
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
